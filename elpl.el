59b5fbe4ed68f9ef9addc1c27e89fa5012ac69c0
Xu Chunyang <mail@xuchunyang.me>
2019-07-02 00:07:07 +0800

    Cleanup trailing parentheses on distinct line

--- elpl.el
+++ elpl.el
@@ -106,8 +106,7 @@
   (interactive)
   (when (eq major-mode 'elpl-mode)
     (let ((comint-buffer-maximum-size 0))
-      (comint-truncate-buffer)
-      )))
+      (comint-truncate-buffer))))
 
 ;;;###autoload
 (defun elpl ()
